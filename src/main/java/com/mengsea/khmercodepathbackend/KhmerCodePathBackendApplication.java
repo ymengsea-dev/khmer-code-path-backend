8e3fb54288746717e793e583248b26128ad27f5a
ymengsea <mrmengsea@gmail.com>
2026-03-11 20:54:20 +0700

    init project

---
+++ src/main/java/com/mengsea/khmercodepathbackend/KhmerCodePathBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mengsea.khmercodepathbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhmerCodePathBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhmerCodePathBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mengsea/khmercodepathbackend/KhmerCodePathBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengsea.khmercodepathbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhmerCodePathBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
